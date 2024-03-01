8aa1f5046a62097dd065f67432c2b98734eb0bc1
oresteslorda <olorda@transactrx.com>
2024-03-01 11:39:01 -0500

    Update gofiber-session.go

--- pkg/gofiber-session/gofiber-session.go
+++ pkg/gofiber-session/gofiber-session.go
@@ -63,7 +63,7 @@ func ProxyAuthRequire(config Config) fiber.Handler {
 
 		//Read URL Querystring
 		onUrl.AppId = q.Get("appid")
- 
+
 		if len(strings.TrimSpace(onUrl.AppId)) == 0 {
 			log.Printf(" ERROR Unable to find appid inside URL Query")
 			ctx.Status(http.StatusBadRequest).JSON(&fiber.Map{"status": http.StatusBadRequest, "code": "Invalid-Query-String", "message": "Invalid Access"})
