d53f082f63a358990162e8ba6c2cf0e4b0915d1a
oresteslorda <olorda@transactrx.com>
2024-03-01 11:10:11 -0500

    adding proxy auth to is

--- pkg/gofiber-session/gofiber-session.go
+++ pkg/gofiber-session/gofiber-session.go
@@ -46,6 +46,95 @@ func SessionRequire(config Config) fiber.Handler {
 
 }
 
+func ProxyAuthRequire(config Config) fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+
+		store := config.Session.Get(ctx)
+		defer store.Save()
+
+		onUrl := IdentityObj{}
+
+		q, err := url.ParseQuery(string(ctx.Request().URI().QueryString()))
+		if err != nil {
+			log.Printf(" ERROR parsing query: %v", err)
+			ctx.Status(http.StatusBadRequest).JSON(&fiber.Map{"status": http.StatusBadRequest, "code": "Invalid-Query-String", "message": "Invalid Access"})
+			return fmt.Errorf("unauthorized Access")
+		}
+
+		//Read URL Querystring
+		onUrl.AppId = q.Get("appid")
+		onUrl.TrxISAT = q.Get("TRX-ISAT")
+
+		if len(strings.TrimSpace(onUrl.TrxISAT)) == 0 || len(strings.TrimSpace(onUrl.AppId)) == 0 {
+			log.Printf(" ERROR Unable to find appid/TRX-ISAT inside URL Query String: %v", err)
+			ctx.Status(http.StatusBadRequest).JSON(&fiber.Map{"status": http.StatusBadRequest, "code": "Invalid-Query-String", "message": "Invalid Access"})
+			return fmt.Errorf("unauthorized Access")
+		}
+
+		log.Print("New Session, verify identity with IdentityService!")
+
+		//Verify identity
+		loginUrl := fmt.Sprintf("%s?appid=%s", config.LoginUrl, onUrl.AppId)
+
+		if len(strings.TrimSpace(onUrl.TrxISAT)) == 0 {
+			log.Printf("Redirect loginUrl: %s", loginUrl)
+			return ctx.Redirect(loginUrl)
+		}
+
+		req, _ := http.NewRequest(http.MethodPost, config.CredentialUrl, bytes.NewBuffer([]byte(onUrl.TrxISAT)))
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Printf("Error user authentication: %v", err)
+			return ctx.Redirect(loginUrl)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
+			log.Printf("Identity Status Code %v", resp.StatusCode)
+			return ctx.Redirect(loginUrl)
+		}
+
+		//Read session details data from identity resp
+		userSessionDetail := SessionDetails{}
+		err = json.NewDecoder(resp.Body).Decode(&userSessionDetail)
+		if err != nil {
+			log.Printf("Session Details resp error %v", err)
+			return ctx.Redirect(loginUrl)
+		}
+
+		//Save session
+		if len(onUrl.View) > 0 {
+			userSessionDetail.AppView = onUrl.View
+			store.Set("VIEW", onUrl.View)
+			store.Set("AppView", userSessionDetail.AppView)
+		}
+
+		cookieTk := ctx.Cookies(config.CookieName, INVALID_ACCESS)
+		//Check cookie to authorize valid call's source
+		if cookieTk == INVALID_ACCESS {
+			ctx.Status(http.StatusUnauthorized).JSON(&fiber.Map{"status": http.StatusBadRequest, "code": http.StatusUnauthorized, "message": "Unauthorized Access"})
+			return fmt.Errorf("unauthorized Access")
+		}
+
+		log.Printf("****AuthRequire TRX_CUST_NUM: %s", cookieTk)
+
+		store.Set(STORED_COOKIE_NAME, cookieTk)
+		store.Set("AccountId", userSessionDetail.AccountId)
+		store.Set("FirstName", userSessionDetail.FirstName)
+		store.Set("LastName", userSessionDetail.LastName)
+		store.Set("DefaultProfile", userSessionDetail.DefaultProfile)
+		store.Set("UserId", userSessionDetail.UserId)
+
+		if err := ctx.Next(); err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
 func AuthRequire(config Config) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
 
