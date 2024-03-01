8037f5589a651964dab48a8e847cc1ca39946dda
oresteslorda <olorda@transactrx.com>
2024-03-01 11:22:10 -0500

    Update gofiber-session.go

--- pkg/gofiber-session/gofiber-session.go
+++ pkg/gofiber-session/gofiber-session.go
@@ -66,7 +66,7 @@ func ProxyAuthRequire(config Config) fiber.Handler {
 		onUrl.TrxISAT = q.Get("TRX-ISAT")
 
 		if len(strings.TrimSpace(onUrl.TrxISAT)) == 0 || len(strings.TrimSpace(onUrl.AppId)) == 0 {
-			log.Printf(" ERROR Unable to find appid/TRX-ISAT inside URL Query String: %v", err)
+			log.Printf(" ERROR Unable to find appid/TRX-ISAT inside URL Query")
 			ctx.Status(http.StatusBadRequest).JSON(&fiber.Map{"status": http.StatusBadRequest, "code": "Invalid-Query-String", "message": "Invalid Access"})
 			return fmt.Errorf("unauthorized Access")
 		}
